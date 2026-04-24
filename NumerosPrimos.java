e809dd2d09ba6741d858d74aed90a33cfe575a07
Jacinto <jacinto.ramos@cifpvilladeaguimes.es>
2026-04-24 08:52:11 +0100

    Hacemos los comentarios pertinentes y generamos el Javadoc

--- NumerosPrimos.java
+++ NumerosPrimos.java
@@ -1,8 +1,16 @@
 import java.util.Scanner;
 
+/**
+ * Clase que hace un recorrido entre dos números y muestra los que son primos
+ */
+
 public class NumerosPrimos {
     static Scanner t = new Scanner(System.in);
 
+    /**
+     * Base del programa principal que ejecuta el programa
+     * @param args argumentos vacíos que no se usan
+     */
     public static void main(String[] args) {
         int numero1, numero2;
         System.out.print("Introduzca primer numero: ");
@@ -12,6 +20,12 @@ public class NumerosPrimos {
         listaPrimos(numero1, numero2);
     }
 
+    /**
+     * Método que devuelva una lista de números primos después de recorrer los 2 números pasados por el usuario
+     *
+     * @param numero1 primer número que introduce el usuario
+     * @param numero2 segundo número que introduce el usuario
+     */
     private static void listaPrimos(int numero1, int numero2) {
         boolean primos;
         int mayor, menor;
